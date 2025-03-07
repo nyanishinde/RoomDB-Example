a914e3e865dd7c22d2638313dde047fbc42f3b55
nyanishinde <nyanishinde10@gmail.com>
2025-03-07 09:47:23 +0530

    Added 2 empty view Activity and navigated from main activity to them

--- app/src/main/java/com/example/roomdbexampleapp/ViewAllEmployee.kt
+++ app/src/main/java/com/example/roomdbexampleapp/ViewAllEmployee.kt
@@ -1,37 +1,20 @@
 package com.example.roomdbexampleapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var btnAddEmployee: Button
-    private lateinit var btnViewAllEmployee: Button
-
+class ViewAllEmployee : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_view_all_employee)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        btnAddEmployee = findViewById(R.id.addEmployee)
-        btnViewAllEmployee = findViewById(R.id.viewAllEmployee)
-
-        btnAddEmployee.setOnClickListener {
-            startActivity(Intent(this,AddEmployee::class.java))
-        }
-        btnViewAllEmployee.setOnClickListener {
-            startActivity(Intent(this,ViewAllEmployee::class.java))
-        }
-
     }
 }
